f8c8b1cea29b67ff0a387440d6ef6c93dfe0c0b6
RedPandaMC <jurreandenys@gmail.com>
2023-03-30 18:56:03 +0200

    aaaaaaa

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 def dir_checker():
     """
     This function is used to check if the needed
-    directories are already created and if not 
-    they shall be created.
+    directories are already created
     """
     if not os.path.exists("_site"):
         os.mkdir("./_site")
@@ -13,3 +12,4 @@ def dir_checker():
 
 if __name__ == "__main__":
     dir_checker()
+    open("./_input-markdown-document-here/site_base.md","w",encoding="UTF-8")
