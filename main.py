f928f06c0b967403b33a6dd257824847920c50df
RedPandaMC <jurreandenys@gmail.com>
2023-03-30 20:47:36 +0200

    (:

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+
 def dir_checker():
     """
     This function is used to check if the needed
@@ -9,7 +10,31 @@ def dir_checker():
         os.mkdir("./_site")
     if not os.path.exists("_input-markdown-document-here"):
         os.mkdir("./_input-markdown-document-here")
+        basefile = open("./_input-markdown-document-here/site_base.md","w",encoding="UTF-8")
+        basefile.write("Please only write markdown and YAML here.")
+        basefile.close()
+
+
+def splitfile():
+    """
+    This function is used to open the given markdown
+    file and splits it into the yaml part and the markdown
+    part
+    """
+    sitemarkdown = open("./_input-markdown-document-here/site_base.md","r",encoding="UTF-8")
+    smlines = sitemarkdown.read()
+    try:
+        yaml_end = smlines.index('---', 4) + 4
+
+        frontmatter = smlines[:yaml_end]
+        backmatter = smlines[yaml_end:]
+    except ValueError:
+        frontmatter = ''
+        backmatter = ''
+
+    return {'yaml':frontmatter,'md':backmatter}
+
+
 
 if __name__ == "__main__":
     dir_checker()
-    open("./_input-markdown-document-here/site_base.md","w",encoding="UTF-8")
