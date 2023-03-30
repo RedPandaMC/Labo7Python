bda66a6b49e8b87c1efcff609eea795bfb237924
RedPandaMC <jurreandenys@gmail.com>
2023-03-30 18:49:04 +0200

    go commit die

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import os
+
+def dir_checker():
+    """
+    This function is used to check if the needed
+    directories are already created and if not 
+    they shall be created.
+    """
+    if not os.path.exists("_site"):
+        os.mkdir("./_site")
+    if not os.path.exists("_input-markdown-document-here"):
+        os.mkdir("./_input-markdown-document-here")
+
+if __name__ == "__main__":
+    dir_checker()
