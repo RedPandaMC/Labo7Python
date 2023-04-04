c3e9c4b20c3b79a9dfbed053b87e5ef37fd9532a
RedPandaMC <jurreandenys@gmail.com>
2023-04-04 14:39:06 +0200

    go commit die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-import yaml2html as y2h
-
+import md2html as md2h
 
 def dir_checker():
     """
@@ -14,40 +13,10 @@ def dir_checker():
         basefile = open(
             "./_input-markdown-document-here/site_base.md", "w", encoding="UTF-8"
         )
-        basefile.write(
-        """---\nDate: 01/01/2000\n\
-Title: Main\nAuthor: AuthorName\nTemplateName: Name\nPageType: PageType\n---
-        \n# start typing\n"""
-        )
+        basefile.write("---\nDate: 01/01/2000\nTitle: Main\nAuthor: AuthorName\nTemplateName: Name\nPageType: PageType\n---\n# start typing\n")
         basefile.close()
         exit("Created directories, please rerun the script.")
 
-
-def splitfile():
-    """
-    This function is used to open the given markdown
-    file and splits it into the yaml part and the markdown
-    part
-    """
-    sitemarkdown = open(
-        "./_input-markdown-document-here/site_base.md", "r", encoding="UTF-8"
-    )
-    smlines = sitemarkdown.read()
-    try:
-        yaml_end = smlines.index("---", 1)
-        frontmatter = smlines[3:yaml_end]
-        backmatter = smlines[yaml_end + 3 :]
-    except ValueError:
-        yaml_end = 0
-        frontmatter = ""
-        backmatter = smlines[yaml_end:]
-        print("No yaml found in markdown document")
-    frontmatter = frontmatter.replace("\n", ",").removeprefix(",").removesuffix(",")
-    backmatter = backmatter.replace("\n", "")
-    return {"yaml": frontmatter, "md": backmatter}
-
-
 if __name__ == "__main__":
     dir_checker()
-    file_dic = splitfile()
-    y2h.yaml2html_converter(file_dic["yaml"])
+    md2h.md2html_converter()
---
+++ md2html.py
@@ -0,0 +1,103 @@
+import os
+import shutil
+import jinja2 as j2
+import markdown
+
+
+def splitfile():
+    """
+    This function is used to open the given markdown
+    file and splits it into the yaml part and the markdown
+    part
+    """
+    sitemarkdown = open(
+        "./_input-markdown-document-here/site_base.md", "r", encoding="UTF-8"
+    )
+    smlines = sitemarkdown.read()
+    try:
+        yaml_end = smlines.index("---", 1)
+        frontmatter = smlines[3:yaml_end]
+        backmatter = smlines[yaml_end + 3 :]
+    except ValueError:
+        yaml_end = 0
+        frontmatter = ""
+        backmatter = smlines[yaml_end:]
+        print("No yaml found in markdown document")
+    frontmatter = frontmatter.replace("\n", ",").removeprefix(",").removesuffix(",")
+    return {"yaml": frontmatter, "md": backmatter}
+
+
+def string_to_dict(string: str):
+    """
+    This function turns a yaml string into a yaml dictionary
+    """
+    pairs = string.split(",")
+    dire = {}
+    for pair in pairs:
+        key, value = pair.split(":")
+        key = key.strip()
+        value = value.strip().lower()
+        dire[key] = value
+    return dire
+
+
+def get_html_files(template_name: str):
+    """
+    This function gives returns a dictionary with all
+    our html files in a given template group
+    """
+    html_files = {}
+    for file in os.listdir(f"_html-templates/{template_name}"):
+        if file.endswith(".html"):
+            file_name = file.removesuffix(".html")
+            html_files[file_name] = file
+    return html_files
+
+
+def get_template_names():
+    """
+    This function gives returns a list of all of our templates
+    """
+    html_files = []
+    for directory in os.listdir("_html-templates"):
+        if os.path.isdir(os.path.join("_html-templates", directory)):
+            html_files.append(directory)
+    return html_files
+
+
+def cp_mv_specifiedfile(yaml_dic: dict):
+    """
+    Copies a specified file and moves it to directory _site, if it doesn't exist
+    """
+    if not os.path.isfile(os.path.join("_site", yaml_dic["PageType"]+".html")):
+        template = yaml_dic["TemplateName"].removesuffix(".html")
+        html_files = get_html_files(template_name=template)
+
+        source_file = f"_html-templates/{template}/{html_files[yaml_dic['PageType']]}"
+        target_dir = "_site"
+        shutil.copy2(source_file, target_dir)
+
+
+def markdown2html(file_dic: dict):
+    """
+    Returns all html in specified file + added html from markdown
+    """
+    html = markdown.markdown(file_dic["md"])
+    return html
+
+def md2html_converter():
+    """
+    Converts yaml to html
+    """
+    file_dic = splitfile()
+    yaml_dic = string_to_dict(file_dic["yaml"])
+
+    template = yaml_dic["TemplateName"].removesuffix(".html")
+    templates = get_template_names()
+    if template not in templates:
+        exit("Template doesn't exist in _html-templates")
+    cp_mv_specifiedfile(yaml_dic)
+
+    full_dic = yaml_dic
+    full_dic["content"] = markdown2html(file_dic)
+    print(full_dic)
